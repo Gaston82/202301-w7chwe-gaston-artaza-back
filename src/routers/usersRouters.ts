450d794c341d5c69c57d2b79ae209ebcc0210bbe
Gaston82 <cabezaartaza@hotmail.com>
2023-02-26 13:14:43 +0100

    Create checkFields middleware

--- src/routers/usersRouters.ts
+++ src/routers/usersRouters.ts
@@ -1,6 +1,7 @@
 import { check } from "express-validator";
 import { Router } from "express";
 import { getUsers, registerUser } from "../controllers/userControllers.js";
+import { checkFields } from "../middlewares/check-fields.js";
 
 const usersRouter = Router();
 
@@ -8,10 +9,12 @@ usersRouter.post(
   "/register",
   [
     check("username", "Username is required").not().isEmpty(),
+    check("password", "Password is required").not().isEmpty(),
     check("password", "Password should have 6 characters minimun").isLength({
       min: 6,
     }),
     check("email", "Email not valid").isEmail(),
+    checkFields,
   ],
   registerUser
 );
